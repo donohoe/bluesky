2391ad3ee9054b0330b14baac868b87d14a84e92
Michael Donohoe <donohoe@gmail.com>
2023-05-14 12:17:25 -0400

    Fix the path to sample

--- examples/post-embed-link.php
+++ examples/post-embed-link.php
@@ -3,23 +3,14 @@
 include '../bluesky.php';
 
 $text = "Bluesky does not have its own oembed service so you have full control over preview experience";
-$card = array(
-    'uri'         => 'https://github.com/donohoe/blueksy',
-    'title'       => 'A simple (and very basic) PHP library for Bluesky',
-    'description' => 'This minimal PHP client allows you to post text, links, and images to Bluesky',
-);
-$thumbnail = '/social-card.jpg';
-
-// Card to be Embedded
 
 $embed = array(
     'type'        => 'link',
-    'uri'         => $card['uri'],
-    'title'       => $card['title'],
-    'description' => $card['description'],
-    'thumb'       => $thumbnail
+    'uri'         => 'https://github.com/donohoe/blueksy',
+    'title'       => 'A simple (and very basic) PHP library for Bluesky',
+    'description' => 'This minimal PHP client allows you to post text, links, and images to Bluesky',
+    'thumb'       => '../examples/social-card.jpg'
 );
-
 
 $bluesky = new Bluesky;
 $bluesky->setAccount('../account.json');
